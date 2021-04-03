b6aab4ab14ba14a2ffbb9b23da1423a5fc92e429
thomas5566 <thomas19840826@gmail.com>
2021-04-03 14:49:48 +0800

    Added custom user profile model

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# tells django to look at profiles_api and find model UserProfile
+AUTH_USER_MODEL = 'profiles_api.UserProfile' 
